c6b7a823fd0f74c81ad80e4d23bc7f7c657341bf
flubbid <brendanwilson1@gmail.com>
2019-06-19 10:42:12 -0500

    fixes error on usercreation form in the signup def

--- Minerva/main_app/views/main.py
+++ Minerva/main_app/views/main.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm 
 
 class SignUpView(TemplateView):
     template_name = 'Registration/signup.html'
